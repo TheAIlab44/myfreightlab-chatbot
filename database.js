4b30b85c7c41cc8e8d90c76438e45c1c556a12d9
TheAIlab44 <huot795@gmail.com>
2025-06-04 11:12:34 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 // Initialisation du client Supabase (supabaseLib est défini par le CDN)
 const SUPABASE_URL     = "https://asjqmzgcajcizutrldqw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzanFtemdjYWpjaXp1dHJsZHF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMTY1MjAsImV4cCI6MjA1NjU5MjUyMH0.8AGX4EI6F88TYrs1aunsFuwLWJfj3Zf_SJW1Y1tiTZc";
-const supabase = supabaseLib.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 document.addEventListener("DOMContentLoaded", async () => {
   // ————— Paramètres & états —————
